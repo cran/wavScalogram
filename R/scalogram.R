c3a2439faac8be29a3ccafec2bdee492c56a25db
Vicente J. Bolos <vicente.bolos@uv.es>
2019-05-24 09:00:02 +0000

    version 1.0.0

--- R/scalogram.R
+++ R/scalogram.R
@@ -24,7 +24,10 @@
 #'                  border_effects = c("BE", "INNER", "PER", "SYM"),
 #'                  energy_density = TRUE,
 #'                  makefigure = TRUE,
-#'                  figureperiod = TRUE)
+#'                  figureperiod = TRUE,
+#'                  xlab = NULL,
+#'                  ylab = "Scalogram",
+#'                  main = "Scalogram")
 #'
 #' @param signal A vector containing the signal whose scalogram is wanted.
 #' @param dt Numeric. The time step of the signal.
@@ -40,8 +43,8 @@
 #' @param wparam The corresponding nondimensional parameter for the wavelet function
 #' (Morlet, DoG or Paul).
 #' @param waverad Numeric. The radius of the wavelet used in the computations for the cone
-#' of influence. If it is not specified, it is computed by \code{wavelet_radius} for DoG
-#' and Paul wavelets. For Haar and Morlet it is assumed to be 1 and 3 respectively.
+#' of influence. If it is not specified, it is asumed to be \eqn{\sqrt{2}} for Morlet and DoG,
+#' \eqn{1/\sqrt{2}} for Paul and 0.5 for Haar.
 #' @param border_effects String, equal to "BE", "INNER", "PER" or "SYM", which indicates
 #' how to manage the border effects which arise usually when a convolution is performed on
 #' finite-lenght signals.
@@ -59,13 +62,19 @@
 #' @param makefigure Logical. If TRUE (default), a figure with the scalogram is plotted.
 #' @param figureperiod Logical. If TRUE (default), periods are used in the figure instead
 #' of scales.
+#' @param xlab A string giving a custom X axis label. If NULL (default) the X label is
+#' either "Scale" or "Period" depending on the value of \code{figureperiod}.
+#' @param ylab A string giving a custom Y axis label.
+#' @param main A string giving a custom main title for the figure.
 #'
 #' @return A list with the following fields:
 #' \itemize{
 #' \item \code{scalog}: A vector of length \code{length(scales)}, containing the values of
 #' the scalogram at each scale.
 #' \item \code{scales}: The vector of scales.
-#' \item \code{fourier_factor}: A factor for converting scales to periods.
+#' \item \code{energy}: If \code{energy_density} is TRUE, it is the \eqn{L^2} norm of
+#' \code{scalog}.
+#' \item \code{fourierfactor}: A factor for converting scales into periods.
 #' }
 #'
 #' @examples
@@ -96,19 +105,21 @@ scalogram <-
            border_effects = c("BE", "INNER", "PER", "SYM"),
            energy_density = TRUE,
            makefigure = TRUE,
-           figureperiod = TRUE) {
+           figureperiod = TRUE,
+           xlab = NULL,
+           ylab = "Scalogram",
+           main = "Scalogram") {
 
   wname <- toupper(wname)
   wname <- match.arg(wname)
 
   if (is.null(waverad)) {
-    if (wname == "MORLET") {
-      waverad <- 3
-    } else if ((wname == "HAAR") || (wname == "HAAR2")) {
+    if ((wname == "MORLET") || (wname == "DOG")) {
+      waverad <- sqrt(2)
+    } else if (wname == "PAUL") {
+      waverad <- 1 / sqrt(2)
+    } else { # HAAR
       waverad <- 0.5
-    } else {
-      waverad <- wavelet_radius(wname = wname, wparam = wparam)
-      waverad <- waverad$left
     }
   }
 
@@ -137,11 +148,10 @@ scalogram <-
                    border_effects = border_effects_cwt,
                    makefigure = FALSE)
 
-  coefs <- cwt$coefs
   scalesdt <- cwt$scales
   scales <- scalesdt / dt
-
   ns <- length(scales)
+  coefs <- matrix(cwt$coefs, nrow = nt, ncol = ns)
 
   scalog <- numeric(ns)
 
@@ -155,7 +165,8 @@ scalogram <-
         print("We need more times for these scales")
         scalog <- scalog[1:(i - 1)]
         scales <- scales[1:(i - 1)]
-        ns <- i-1
+        scalesdt <- scalesdt[1:(i - 1)]
+        ns <- i - 1
         break
       }
       scalog[i] <- sqrt(sum(abs(coefs[nt_ini:nt_end, i]) ^ 2))
@@ -169,27 +180,29 @@ scalogram <-
 
   }
 
+  energy <- NA
   if (energy_density) {
     scalog <- scalog / sqrt(scalesdt)
+    energy <- sqrt(sum(scalog ^ 2))
   }
 
-  fourier_factor <- cwt$fourier_factor
+  fourierfactor <- cwt$fourierfactor
 
   if (makefigure) {
 
     if (figureperiod) {
-      X <- fourier_factor * scalesdt
-      xlab <- "Period"
+      X <- fourierfactor * scalesdt
+      if (is.null(xlab)) xlab <- "Period"
     } else {
       X <- scalesdt
-      xlab <- "Scale"
+      if (is.null(xlab)) xlab <- "Scale"
     }
 
     if (powerscales) {
-      plot(log2(X), scalog, type = "l", xlab = xlab, ylab = "Scalogram", main = "Scalogram", xaxt = "n")
+      plot(log2(X), scalog, type = "l", xlab = xlab, ylab = ylab, main = main, xaxt = "n")
       axis(side = 1, at = floor(log2(X)), labels = 2^(floor(log2(X))))
     } else {
-      plot(X, scalog, type = "l", xlab = xlab, ylab = "Scalogram", main = "Scalogram", xaxt = "n")
+      plot(X, scalog, type = "l", xlab = xlab, ylab = ylab, main = main, xaxt = "n")
       axis(side = 1, at = X[1 + floor((0:8) * (ns - 1) / 8)])
     }
 
@@ -198,7 +211,8 @@ scalogram <-
 return(list(
   scalog = scalog,
   scales = scalesdt,
-  fourier_factor = fourier_factor
+  energy = energy,
+  fourierfactor = fourierfactor
   ))
 
 }
--- R/wsd.R
+++ R/wsd.R
@@ -10,7 +10,8 @@
 #'            windowrad = NULL,
 #'            rdist = NULL,
 #'            delta_t = NULL,
-#'            normalize = FALSE,
+#'            normalize = c("NO", "ENERGY", "MAX", "SCALE"),
+#'            refscale = NULL,
 #'            wname = c("MORLET", "DOG", "PAUL", "HAAR", "HAAR2"),
 #'            wparam = NULL,
 #'            waverad = NULL,
@@ -23,7 +24,10 @@
 #'            parallel = FALSE,
 #'            makefigure = TRUE,
 #'            time_values = NULL,
-#'            figureperiod = TRUE)
+#'            figureperiod = TRUE,
+#'            xlab = "Time",
+#'            ylab = NULL,
+#'            main = "-log2(WSD)")
 #'
 #' @param signal1 A vector containing the first signal.
 #' @param signal2 A vector containing the second signal (its length should be equal to
@@ -33,20 +37,25 @@
 #' and the number of suboctaves per octave for constructing power 2 scales (following
 #' Torrence and Compo 1998). If NULL, they are automatically computed.
 #' @param windowrad Integer. Time radius for the windows, measured in \code{dt}. By
-#' default, it is set to \eqn{celing(length(signal1) / 20)}.
+#' default, it is set to \eqn{ceiling(length(signal1) / 20)}.
 #' @param rdist Integer. Log-scale radius for the windows measured in suboctaves. By
 #' default, it is set to \eqn{ceiling(length(scales) / 20)}.
 #' @param delta_t Integer. Increment of time for the construction of windows central
 #' times, measured in \code{dt}. By default, it is set to
 #' \eqn{ceiling(length(signal1) / 256)}.
-#' @param normalize Logical. Set to TRUE if the signals use different units.
+#' @param normalize String, equal to "NO", "ENERGY", "MAX" or "SCALE". If "ENERGY", signals are
+#' divided by their respective energies. If "MAX", each signal is divided by the maximum
+#' value attained by its scalogram. In these two cases, \code{energy_density} must be TRUE.
+#' Finally, if "SCALE", each signal is divided by their scalogram value at scale
+#' \code{refscale}.
+#' @param refscale Numeric. The reference scale for \code{normalize}.
 #' @param wname A string, equal to "MORLET", "DOG", "PAUL", "HAAR" or "HAAR2". The
 #' difference between "HAAR" and "HAAR2" is that "HAAR2" is more accurate but slower.
 #' @param wparam The corresponding nondimensional parameter for the wavelet function
 #' (Morlet, DoG or Paul).
 #' @param waverad Numeric. The radius of the wavelet used in the computations for the cone
-#' of influence. If it is not specified, it is computed by \code{wavelet_radius} for DoG
-#' and Paul wavelets. For Haar and Morlet it is assumed to be 1 and 3 respectively.
+#' of influence. If it is not specified, it is asumed to be \eqn{\sqrt{2}} for Morlet and DoG,
+#' \eqn{1/\sqrt{2}} for Paul and 0.5 for Haar.
 #' @param border_effects String, equal to "BE", "INNER", "PER" or "SYM",
 #' which indicates how to manage the border effects which arise usually when a convolution
 #' is performed on finite-lenght signals.
@@ -81,20 +90,25 @@
 #'   the scalograms and is chosen in order to make a relative (\eqn{= 0}), absolute
 #'   (\eqn{= 1}) or mix (in \eqn{(0,1)}) difference between scalograms. Default value is
 #'   set to \eqn{0.02}.
-#' @param compensation Numeric in \eqn{[0,1]}. It is an alternative to \code{wscnoise} for
+#' @param compensation Numeric. It is an alternative to \code{wscnoise} for
 #' preventing numerical errors or non-relevant high relative differences when scalogram
-#' values are close to zero (see Bolós et al. 2017).
+#' values are close to zero (see Bolós et al. 2017). It should be a non-negative
+#' relatively small value.
 #' @param energy_density Logical. If TRUE (default), divide the scalograms by the square
 #' root of the scales for convert them into energy density. Note that it does not affect
 #' the results if \code{wscnoise} \eqn{= 0}.
-#' @param parallel Logical. If TRUE (default) uses function \code{parApply} from package
-#' \code{parallel} for the Montecarlo simulations. When FALSE it uses the normal
+#' @param parallel Logical. If TRUE, it uses function \code{parApply} from package
+#' \code{parallel} for the Montecarlo simulations. When FALSE (default) it uses the normal
 #' \code{apply} function.
 #' @param makefigure Logical. If TRUE (default), a figure with the WSD is plotted.
 #' @param time_values A numerical vector of length \code{length(signal)} containing custom
 #' time values for the figure. If NULL (default), it will be computed starting at 0.
 #' @param figureperiod Logical. If TRUE (default), periods are used in the figure instead
 #' of scales.
+#' @param xlab A string giving a custom X axis label.
+#' @param ylab A string giving a custom Y axis label. If NULL (default) the Y label is
+#' either "Scale" or "Period" depending on the value of \code{figureperiod}.
+#' @param main A string giving a custom main title for the figure.
 #'
 #' @importFrom parallel parApply detectCores makeCluster stopCluster
 #' @importFrom abind abind
@@ -116,7 +130,7 @@
 #' \item \code{signif05}: A logical matrix of size \code{length(tcentral)} x
 #' \code{length(scales)}. If TRUE, the corresponding point of the \code{wsd} matrix is in
 #' the 5\% significance.
-#' \item \code{fourier_factor}: A factor for converting scales to periods.
+#' \item \code{fourierfactor}: A factor for converting scales into periods.
 #' \item \code{coi_maxscale}: A vector of length \code{length(tcentral)} containing the
 #' values of the maximum scale from which there are border effects for the respective
 #' central time.
@@ -152,7 +166,8 @@ wsd <-
            windowrad = NULL,
            rdist = NULL,
            delta_t = NULL,
-           normalize = FALSE,
+           normalize = c("NO", "ENERGY", "MAX", "SCALE"),
+           refscale = NULL,
            wname = c("MORLET", "DOG", "PAUL", "HAAR", "HAAR2"),
            wparam = NULL,
            waverad = NULL,
@@ -165,26 +180,31 @@ wsd <-
            parallel = FALSE,
            makefigure = TRUE,
            time_values = NULL,
-           figureperiod = TRUE) {
+           figureperiod = TRUE,
+           xlab = "Time",
+           ylab = NULL,
+           main = "-log2(WSD)") {
 
   #  require(abind)
 
     wname <- toupper(wname)
     wname <- match.arg(wname)
 
-    if (is.null(waverad)) {
-      if (wname == "MORLET") {
-        waverad <- 3
-      } else if ((wname == "HAAR") || (wname == "HAAR2")) {
-        waverad <- 0.5
-      } else {
-        waverad <- wavelet_radius(wname = wname, wparam = wparam)
-        waverad <- waverad$left
-      }
-    }
-
     border_effects <- toupper(border_effects)
     border_effects <- match.arg(border_effects)
+
+    normalize <- toupper(normalize)
+    normalize <- match.arg(normalize)
+
+    if (is.null(waverad)) {
+      if ((wname == "MORLET") || (wname == "DOG")) {
+        waverad <- sqrt(2)
+      } else if (wname == "PAUL") {
+        waverad <- 1 / sqrt(2)
+      } else { # HAAR
+        waverad <- 0.5
+      }
+    }
 
     nt <- length(signal1)
     if (nt != length(signal2)) {
@@ -202,9 +222,12 @@ wsd <-
       print("Windowrad re-adjusted.")
     }
 
+    fourierfactor <- fourier_factor(wname = wname, wparam = wparam)
+
     if (is.null(scaleparam)) {
+      scmin <- 2 / fourierfactor
       scmax <- floor((nt - 2 * windowrad) / (2 * waverad))
-      scaleparam <- c(2 * dt, scmax * dt, ceiling(256 / (log2(scmax) - 1)))
+      scaleparam <- c(scmin * dt, scmax * dt, ceiling(256 / log2(scmax / scmin)))
     }
 
     scalesdt <- pow2scales(scaleparam)
@@ -215,22 +238,51 @@ wsd <-
       rdist <- ceiling(ns / 20)
     }
 
-    ### Scalograms
+    ### Normalization & Scalograms
 
-    scalog1 <-
-      scalogram(signal = signal1, dt = dt,
-                scales = scalesdt, powerscales = FALSE,
-                border_effects = border_effects,
-                energy_density = energy_density,
-                wname = wname, wparam = wparam, waverad = waverad,
-                makefigure = FALSE)
-    scalog2 <-
-      scalogram(signal = signal2, dt = dt,
-                scales = scalesdt, powerscales = FALSE,
-                border_effects = border_effects,
-                energy_density = energy_density,
-                wname = wname, wparam = wparam, waverad = waverad,
-                makefigure = FALSE)
+    if (normalize != "NO") {
+
+      if ((!energy_density) && (normalize != "SCALE")) {
+        warning("For energy or max normalization, energy_density is set to TRUE.")
+        energy_density <- TRUE
+      }
+
+      if (normalize == "SCALE") {
+        if (is.null(refscale)) {
+          stop("No refscale parameter specified for normalization.")
+        } else {
+          scales_aux <- refscale
+        }
+      } else {
+        scales_aux <- scalesdt
+      }
+
+      scalog1 <-
+        scalogram(signal = signal1, dt = dt,
+                  scales = scales_aux, powerscales = FALSE,
+                  border_effects = border_effects,
+                  energy_density = energy_density,
+                  wname = wname, wparam = wparam, waverad = waverad,
+                  makefigure = FALSE)
+      scalog2 <-
+        scalogram(signal = signal2, dt = dt,
+                  scales = scales_aux, powerscales = FALSE,
+                  border_effects = border_effects,
+                  energy_density = energy_density,
+                  wname = wname, wparam = wparam, waverad = waverad,
+                  makefigure = FALSE)
+
+      # Normalization
+
+      if (normalize == "ENERGY") {
+        signal1 <- signal1 / scalog1$energy
+        signal2 <- signal2 / scalog2$energy
+      } else { # MAX or SCALE
+        signal1 <- signal1 / max(scalog1$scalog)
+        signal2 <- signal2 / max(scalog2$scalog)
+      }
+
+    }
 
     ### Windowed scalograms
 
@@ -256,19 +308,6 @@ wsd <-
         makefigure = FALSE)
 
     nwsc <- length(wsc1$tcentral)
-
-    # Normalization
-
-    norm1 <- sqrt(sum(scalog1$scalog ^ 2))
-    if (normalize) {
-      norm2 <- sqrt(sum(scalog2$scalog ^ 2))
-    } else {
-      norm2 <- norm1
-    }
-    scalog1$scalog <- scalog1$scalog / norm1
-    scalog2$scalog <- scalog2$scalog / norm2
-    wsc1$wsc <- wsc1$wsc / norm1
-    wsc2$wsc <- wsc2$wsc / norm2
 
     # Compensation
 
@@ -423,18 +462,17 @@ wsd <-
     ### Make figure
 
     tcentraldt <- wsc1$tcentral
-    fourier_factor <- wsc1$fourier_factor
 
     if (makefigure) {
 
       if (figureperiod) {
-        Y <- fourier_factor * scalesdt
-        coi <- fourier_factor * coi_wsc
-        Yname <- "Period"
+        Y <- fourierfactor * scalesdt
+        coi <- fourierfactor * coi_wsc
+        if (is.null(ylab)) ylab <- "Period"
       } else {
         Y <- scalesdt
         coi <- coi_wsc
-        Yname <- "Scale"
+        if (is.null(ylab)) ylab <- "Scale"
       }
 
       if (is.null(time_values)) {
@@ -457,9 +495,9 @@ wsd <-
         rdist = rdist,
         sig95 = wsdsig95,
         sig05 = wsdsig05,
-        Xname = "Time",
-        Yname = Yname,
-        Zname = "-log2(WSD)"
+        Xname = xlab,
+        Yname = ylab,
+        Zname = main
       )
 
     }
@@ -472,7 +510,7 @@ wsd <-
       rdist = rdist,
       signif95 = wsdsig95,
       signif05 = wsdsig05,
-      fourier_factor = fourier_factor,
+      fourierfactor = fourierfactor,
       coi_maxscale = coi_wsc
     ))
 
